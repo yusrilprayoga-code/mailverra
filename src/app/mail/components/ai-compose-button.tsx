edd45e0305b4e5bb7af0923106db20109fa87536
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-10-09 21:56:23 +0700

    Feat: Update Reply Box with Openai

--- src/app/mail/components/ai-compose-button.tsx
+++ src/app/mail/components/ai-compose-button.tsx
@@ -15,43 +15,51 @@ import { generateEmail } from "./action";
 import { readStreamableValue } from "ai/rsc";
 
 type Props = {
-  isComposing: boolean,
-  onGenerate: (token: string) => void
+  isComposing: boolean;
+  onGenerate: (token: string) => void;
 };
 
 const AiComposeButton = (props: Props) => {
-    const [open, setOpen] = React.useState(false);
-    const [prompt , setPrompt] = React.useState('')
+  const [open, setOpen] = React.useState(false);
+  const [prompt, setPrompt] = React.useState("");
 
-    const aiGenerate = async (prompt: string) => {
-        const {output} = await generateEmail('', prompt)
-        for await (const token of readStreamableValue(output)) {
-            if(token) {
-                console.log(token)
-                props.onGenerate(token)
-            }
-        }
+  const aiGenerate = async () => {
+    const { output } = await generateEmail("", prompt);
+    for await (const token of readStreamableValue(output)) {
+      if (token) {
+        console.log(token);
+        props.onGenerate(token);
+      }
     }
+  };
 
   return (
     <Dialog>
       <DialogTrigger>
-        <Button size='icon' variant={'outline'} onClick={() => setOpen(true)}>
-            <BotIcon className="size-4" />
+        <Button size="icon" variant={"outline"} onClick={() => setOpen(true)}>
+          <BotIcon className="size-4" />
         </Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>
-            AI Smart Compose
-          </DialogTitle>
-          <DialogDescription>
-            Generate a smart reply with AI
-          </DialogDescription>
+          <DialogTitle>AI Smart Compose</DialogTitle>
+          <DialogDescription>Generate a smart reply with AI</DialogDescription>
           <div className="h2"></div>
-          <Textarea placeholder="Type your message here" value={prompt} onChange={(e) => setPrompt(e.target.value)}/>
-            <div className="h-2"></div>
-            <Button onClick={() => { aiGenerate(prompt); setOpen(false); setPrompt('') }}>Generate</Button>
+          <Textarea
+            placeholder="What would you like to compose?"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <div className="h-2"></div>
+          <Button
+            onClick={() => {
+              aiGenerate();
+              setOpen(false);
+              setPrompt("");
+            }}
+          >
+            Generate
+          </Button>
         </DialogHeader>
       </DialogContent>
     </Dialog>
