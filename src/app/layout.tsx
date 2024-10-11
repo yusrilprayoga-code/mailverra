42ef3b70aeb1bc3fff0b4d24ccc34b274b44ed9a
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-10-11 15:37:56 +0700

    Feat: Update Loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,31 @@ import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  ClerkProvider,
+} from "@clerk/nextjs";
 import { ThemeProvider } from "next-themes";
 
 import { TRPCReactProvider } from "@/trpc/react";
 import KBar from "@/components/kbar";
+import { LoaderIcon } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Mailverra - Ai Gmail",
   description:
     "Mailverra is a web application that uses AI to help you manage your Gmail inbox.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
-  keywords: ["Ai Gmail", "Gmail", "AI", "Inbox", "Email", 'Mailverra', 'Mailverra AI'],
+  keywords: [
+    "Ai Gmail",
+    "Gmail",
+    "AI",
+    "Inbox",
+    "Email",
+    "Mailverra",
+    "Mailverra AI",
+  ],
 };
 
 export default function RootLayout({
@@ -23,13 +36,25 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
         <body>
-        <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-          <TRPCReactProvider>
-            {/* <KBar> */}
-            {children}
-            {/* </KBar> */}
-          </TRPCReactProvider>
-        </ThemeProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <TRPCReactProvider>
+              <KBar>
+                <ClerkLoading>
+                  <div
+                    className="flex justify-center items-center h-screen"
+                  > 
+                    <LoaderIcon className="w-10 h-10 animate-spin" />
+                  </div>
+                </ClerkLoading>
+                <ClerkLoaded>{children}</ClerkLoaded>
+              </KBar>
+            </TRPCReactProvider>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
